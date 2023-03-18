924b70ba34aeae6200568bf54f6a6ccbfccee7d9
Leandro Vinicius <112980662+leandro-siqueira-ads@users.noreply.github.com>
2023-03-18 17:57:59 -0300

    Add files via upload

---
+++ Listas/main.py
@@ -0,0 +1,17 @@
+print("Tuple:")
+x = [1,4,2,5,3]
+print(x)
+
+print("List:")
+x = [1,4,2,5,3]
+print(x)
+x.append(6)
+print(x)
+x.append(5)
+print(x)
+x.remove(4)
+
+print("Set:")
+x = {1,5,2,4,3}
+print(x)
+x.add(7)
